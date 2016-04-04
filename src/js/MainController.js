b74e4186a3598e23845104e8e647e63200eacb82
David Welling <david.welling@sstid.com>
2016-04-04 15:10:00 -0400

    initial commit

---
+++ src/js/MainController.js
@@ -0,0 +1,28 @@
+﻿angular.module("app", []);
+(function (module) {
+    module.controller("MainController", serviceName);
+
+    serviceName.$inject = ["$http"];
+
+    function serviceName($http) {
+        var promise = $http.get("ganttData.xml",
+            {
+                transformResponse: function (data) {
+                    // convert the data to JSON and provide
+                    // it to the success function below
+                    var x2js = new X2JS();
+                    var json = x2js.xml_str2json(data);
+                    //return json;
+                    var innerJson = x2js.xml_str2json(json["ajax-response"].contents.__cdata);
+                    return innerJson;
+                }
+            }
+            );
+        promise.then(function(data) {
+            console.log(data);
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+    
+})(angular.module("app"));
